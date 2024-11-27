ab48b5f3e70b8cec848b7bf6c6c1650cdc2122db
Katrine <katrinemichala@gmail.com>
2024-11-27 20:03:32 +0100

    installed nuget

--- AirQualityREST/Luftkvalitet/MeasurmentDbContext.cs
+++ AirQualityREST/Luftkvalitet/MeasurmentDbContext.cs
@@ -1,6 +1,18 @@
-﻿namespace AirQualityREST.Luftkvalitet
+﻿using Luftkvalitet;
+using Microsoft.EntityFrameworkCore;
+
+namespace AirQualityREST.Luftkvalitet
 {
-    public class MeasurmentDbContext
+    public class MeasurmentDbContext : DbContext
     {
+
+        public DbSet<Measurement> Measurements { get; set; }
+
+
+        public MeasurmentDbContext(DbContextOptions<MeasurmentDbContext> options) : base(options)
+        {
+            
+        }
+
     }
 }
